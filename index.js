8e21cecdd6ba7ecfd5458a9f1d1323434fec6bcb
Akash Agarwal <akash.agarwal@pavesoft.co>
2024-05-08 19:38:38 +0530

    new changes for deployment

--- index.js
+++ index.js
@@ -40,10 +40,9 @@
 // module.exports = app;
 import express from "express";
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+export default app;
