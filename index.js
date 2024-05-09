4c51a1eeb35aebc3c54e7cb096cb3d6d546c6a9f
Akash Agarwal <akash.agarwal@pavesoft.co>
2024-05-09 09:47:26 +0530

    change index

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 // index.js
 const express = require("express");
-import cors from "cors";
-import { getMoonSectionData } from "./utils/astrologyCalculations.js";
-
-const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
-app.use(cors());
+const PORT = 4000;
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
 
 app.get("/", (req, res) => {
-  res.send("HI! MY API IS RUNNING 👍");
-
-  //   const moonDetails = getMoonSectionData(new Date());
-  //   res.send({ message: moonDetails });
+  res.send("Hey this is my API running 🥳");
 });
 
-app.post("/custom-feeling", (req, res) => {
-  console.log("Insideeee Custom Feelingggg API -----> ", req.body.date);
-  const moonDetails = getMoonSectionData(req.body.date);
-  res.send({ message: moonDetails });
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
 });
 
+// Export the Express API
 module.exports = app;
