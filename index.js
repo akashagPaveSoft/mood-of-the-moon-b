b633b1d6aa6892cbfd00291dd517b2e138aa09a5
Akash Agarwal <akash.agarwal@pavesoft.co>
2024-05-09 09:42:08 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 import express from "express";
-import sweph, { constants } from "sweph";
 import cors from "cors";
-import {
-  getDetailsOfAllPlanets,
-  calculateNextFeeling,
-  convertDateTimeToJulian,
-  getMoonSectionData,
-  calculatePlanetPositionDetails,
-  ephemerisCalculations,
-} from "./utils/astrologyCalculations.js";
+import { getMoonSectionData } from "./utils/astrologyCalculations.js";
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,15 +8,14 @@ export const app = express();
 
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  // console.log("Inside / req----> ", req);
-  const moonDetails = getMoonSectionData(new Date());
-  // console.log("Moon Details insde index.js -> ", moonDetails);
+  res.send("HI! MY API IS RUNNING 👍");
 
-  res.send({ message: moonDetails });
+  //   const moonDetails = getMoonSectionData(new Date());
+  //   res.send({ message: moonDetails });
 });
 
 app.post("/custom-feeling", (req, res) => {
