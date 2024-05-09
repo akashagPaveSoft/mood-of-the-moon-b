e6e238acc956abd96c385e33916b05047e05bfb7
Akash Agarwal <akash.agarwal@pavesoft.co>
2024-05-09 10:01:33 +0530

    back with the main code

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-// index.js
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import { getMoonSectionData } from "./utils/astrologyCalculations.js";
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
+  const moonDetails = getMoonSectionData(new Date());
+
+  res.send({ message: moonDetails });
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
+app.post("/custom-feeling", (req, res) => {
+  const moonDetails = getMoonSectionData(req.body.date);
+  res.send({ message: moonDetails });
 });
 
-// Export the Express API
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
 module.exports = app;
